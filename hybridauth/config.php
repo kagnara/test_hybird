2aff06f74dc72fe6380074e8d6507be343be9a3f
kagnara <kagnara.kol@gmail.com>
2013-03-14 22:11:50 +0700

    hybridauth

---
+++ hybridauth/config.php
@@ -0,0 +1,72 @@
+<?php
+/*!
+* HybridAuth
+* http://hybridauth.sourceforge.net | http://github.com/hybridauth/hybridauth
+* (c) 2009-2012, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html
+*/
+
+// ----------------------------------------------------------------------------------------
+//	HybridAuth Config file: http://hybridauth.sourceforge.net/userguide/Configuration.html
+// ----------------------------------------------------------------------------------------
+
+return 
+	array(
+		"base_url" => "http://localhost/I5/hybridauth/hybridauth/", 
+
+		"providers" => array ( 
+			// openid providers
+			"OpenID" => array (
+				"enabled" => false
+			),
+
+			"Yahoo" => array ( 
+				"enabled" => false,
+				"keys"    => array ( "id" => "", "secret" => "" ),
+			),
+
+			"AOL"  => array ( 
+				"enabled" => false 
+			),
+
+			"Google" => array ( 
+				"enabled" => false,
+				"keys"    => array ( "id" => "", "secret" => "" ), 
+			),
+
+			"Facebook" => array ( 
+				"enabled" => true,
+				"keys"    => array ( "id" => "155815554576983", "secret" => "dc9af86ca240ac0774ffd358a3616009" ), 
+			),
+
+			"Twitter" => array ( 
+				"enabled" => true,
+				"keys"    => array ( "key" => "", "secret" => "" ) 
+			),
+
+			// windows live
+			"Live" => array ( 
+				"enabled" => false,
+				"keys"    => array ( "id" => "", "secret" => "" ) 
+			),
+
+			"MySpace" => array ( 
+				"enabled" => false,
+				"keys"    => array ( "key" => "", "secret" => "" ) 
+			),
+
+			"LinkedIn" => array ( 
+				"enabled" => true,
+				"keys"    => array ( "key" => "jfzqnuuz7z00", "secret" => "Yiux7sE1T8ZeuFks" ) 
+			),
+
+			"Foursquare" => array (
+				"enabled" => false,
+				"keys"    => array ( "id" => "", "secret" => "" ) 
+			),
+		),
+
+		// if you want to enable logging, set 'debug_mode' to true  then provide a writable file by the web server on "debug_file"
+		"debug_mode" => false,
+
+		"debug_file" => "",
+	);
